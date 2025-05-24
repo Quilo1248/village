8868c7886cd58515b46aedf54612c9cd7d8a4cfd
Quilo1248 <ErikBodenstein@proton.me>
2025-05-24 13:24:34 +0200

    made a timer wheel to select duration updated project casing to match godots guidlines

---
+++ Game/Components/timer donut/display.gd
@@ -0,0 +1,20 @@
+extends TextureProgressBar
+
+
+@export var colors : Array[Color]
+@onready var button: Sprite2D = $offset/slider/Button
+
+func _ready() -> void:
+	set_value_minutes(5)
+
+func set_value_minutes(minutes : float):
+	minutes = clamp(minutes, 5, (colors.size() - 1) * 60)
+	var laps = floor(minutes/60)
+	var relitive_value = int(minutes) % 60
+	
+	value = relitive_value
+	
+	tint_under = colors[clamp(laps, 0, colors.size())]
+	tint_progress = colors[clamp(laps + 1, 1, colors.size() - 1)]
+	
+	button.modulate = colors[clamp(laps + 1, 1, colors.size() - 1)]
---
+++ Game/Components/timer donut/slider.gd
@@ -0,0 +1,30 @@
+extends Button
+
+
+var held : bool = false
+@onready var offset: Node2D = $".."
+@onready var donut_display: TextureProgressBar = $"../.."
+
+
+func _ready() -> void:
+	offset.global_position = donut_display.global_position + donut_display.size / Vector2(2,2)
+	offset.rotation_degrees = -60
+
+
+func _on_button_down() -> void:
+	held = true
+
+
+func _on_button_up() -> void:
+	held = false
+
+
+func _process(delta: float) -> void:
+	if not held:
+		return
+	else:
+		offset.look_at(get_global_mouse_position())
+		offset.rotation_degrees = clamp(offset.rotation_degrees, -60, \
+		((donut_display.colors.size() - 1) * 360) - 90)
+		
+		donut_display.set_value_minutes((offset.rotation_degrees + 90)/6)
