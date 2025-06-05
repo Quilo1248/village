73c0f7f9d11dcb538614f6f54bd683749f76d92b
Quilo1248 <ErikBodenstein@proton.me>
2025-06-05 14:58:47 +0200

    make elements disapear when timer started

--- Game/Scenes/TimerManager/display_manager.gd
+++ Game/Scenes/TimerManager/display_manager.gd
@@ -1,9 +1,27 @@
 extends Node
 
+enum TimerState {
+	SET,
+	TIMER,
+	STOPWATCH,
+}
 @onready var donut_timer: AspectRatioContainer = $"../DonutTimer"
 @onready var time_display: Label = $"../DonutTimer/TimeDisplay"
+@onready var selector: Control = $"../Selector"
+@onready var start: Button = $"../Start"
 
 func update_displays(h, m, s):
 	donut_timer.update_displays()
 	time_display.update_display(h, m, s)
+	hide_displays(donut_timer.current_state)
 	
+
+func hide_displays(state : TimerState):
+	if state == TimerState.SET:
+		selector.show()
+		time_display.show()
+		donut_timer.show()
+		start.show()
+	else:
+		selector.hide()
+		start.hide()
--- Game/Scenes/TimerManager/timer_manager.gd
+++ Game/Scenes/TimerManager/timer_manager.gd
@@ -21,3 +21,4 @@ func _on_selector_selection_changed(selection) -> void:
 
 func _on_start_pressed() -> void:
 	donut_timer.start_timer(donut_timer.TimerState[selector.selected])
+	display_manager.update_displays(donut_timer.current_hours, donut_timer.current_minutes, donut_timer.current_seconds)
