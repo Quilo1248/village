118422ba7f3b4f70f4889500f936208220c23df9
Quilo1248 <ErikBodenstein@proton.me>
2025-06-17 13:26:01 +0200

    added tag default colors for the custom ColorPickerHex and made tag display update colors on color change

---
+++ Game/Components/TagEditor/tag_color_manager.gd
@@ -0,0 +1,17 @@
+extends VBoxContainer
+
+@onready var tag: Button = $Tag
+
+
+func _on_background_color_picker_hex_color_changed(color: Color) -> void:
+	tag.background_color = color
+	update_tag()
+
+
+func _on_text_color_picker_hex_color_changed(color: Color) -> void:
+	tag.text_color = color
+	update_tag()
+
+
+func update_tag():
+	tag.update_tag()
