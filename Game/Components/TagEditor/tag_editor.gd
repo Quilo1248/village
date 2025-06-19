50d5acb265324462ca9e6fbd249e1f128c7768af
Quilo1248 <ErikBodenstein@proton.me>
2025-06-19 13:16:42 +0200

    Made tags display and refresh when tags are created

--- Game/Components/TagEditor/tag_editor.gd
+++ Game/Components/TagEditor/tag_editor.gd
@@ -1,7 +1,7 @@
 extends CanvasLayer
 
 signal selected(canceled : bool, title : String, description : String, background_color : Color, text_color : Color)
-
+signal tag_created
 @onready var title: LineEdit = $darken/Panel/VBoxContainer/EditorManager/TagTitlesManager/Title
 @onready var description: TextEdit = $darken/Panel/VBoxContainer/EditorManager/TagTitlesManager/Description
 @onready var background_color_picker_hex: VBoxContainer = $darken/Panel/VBoxContainer/EditorManager/TagColorManager/BackgroundColor/BackgroundColorPickerHex
@@ -23,4 +23,5 @@ func _on_cancel_pressed() -> void:
 
 func _on_save_pressed() -> void:
 	emit_signal("selected", false, title.text, description.text, background_color_picker_hex.current_color, text_color_picker_hex.current_color)
+	emit_signal("tag_created")
 	hide()
--- Game/Components/TagSelector/tag_selector.gd
+++ Game/Components/TagSelector/tag_selector.gd
@@ -1,10 +1,35 @@
 extends CanvasLayer
 
+const TAG = preload("res://Game/Components/Tag/tag.tscn")
+@onready var tags_container: HFlowContainer = $Panel/VBoxContainer/ScrollTags/TagsContainer
+
 
 func popup():
+	refresh()
 	show()
-	
 
 
 func _on_x_pressed() -> void:
 	hide()
+
+
+func refresh():
+	for child in tags_container.get_children():
+		if child.is_in_group("tag"):
+			child.queue_free()
+	
+	SaveLoad._load()
+	var tags = SaveLoad.SaveFileData.tags
+	
+	for i in tags.size():
+		var tag = TAG.instantiate()
+		tag.title = tags[i].title
+		tag.description = tags[i].description
+		tag.background_color = tags[i].background_color
+		tag.text_color = tags[i].text_color
+		
+		tags_container.add_child(tag)
+
+
+func _on_tag_editor_tag_created() -> void:
+	refresh()
