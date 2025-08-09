67dc617471841fd1ae4cfe8f8357a8b51038e252
Quilo1248 <ErikBodenstein@proton.me>
2025-08-09 09:32:02 +0200

    DELETE EVERYTHING. WE STARTIN OVER!

--- Game/Resources/custom resources/Tags.gd
+++
@@ -1,8 +0,0 @@
-extends Resource
-class_name Tag
-
-@export var title : String
-@export var description : String
-@export var background_color : Color
-@export var text_color : Color
-@export var pinned : bool
--- Game/Scripts/Saving/SaveData.gd
+++
@@ -1,7 +0,0 @@
-extends Resource
-class_name SaveDataResource
-
-
-@export var coins : int = 0
-@export var sessions : Array[Session]
-@export var tags : Array[Tag]
